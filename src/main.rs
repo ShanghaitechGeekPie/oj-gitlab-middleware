89b52c1d15b4240a77b592c1084dfa7d19a7c8ed
llk89 <27812632+llk89@users.noreply.github.com>
2019-02-03 16:45:58 +0800

    Fix copyright
    Signed-off-by: llk89 <27812632+llk89@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 /*
- * Copyright (c)  2019,  llk89.
+ *  Copyright (c) 2018-2019, llk89.
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as
