3ae82d62306fe9be12f8cf03dbda9668ef074844
llk89 <27812632+llk89@users.noreply.github.com>
2019-02-07 23:34:32 +0800

    Fix migration
    Signed-off-by: llk89 <27812632+llk89@users.noreply.github.com>

--- entry-point.sh
+++ entry-point.sh
@@ -12,7 +12,7 @@ export ROCKET_DATABASES="{mysql={url=mysql://$GITLAB_MIDDLEWARE_DB_USER:$GITLAB_
 
 MIGRATIONS=$(ls setup | awk '{print "source " $0 ";"}')
 
-mysql -u $GITLAB_MIDDLEWARE_DB_USER -p $GITLAB_MIDDLEWARE_DB_PASS $GITLAB_MIDDLEWARE_DB_HOST:$GITLAB_MIDDLEWARE_DB_PORT \
-    -e "use ${GITLAB_MIDDLEWARE_DB_NAME}; $MIGRATIONS; exec setup_1;"
+mysql -u $GITLAB_MIDDLEWARE_DB_USER -p $GITLAB_MIDDLEWARE_DB_PASS -h $GITLAB_MIDDLEWARE_DB_HOST -P $GITLAB_MIDDLEWARE_DB_PORT \
+    GITLAB_MIDDLEWARE_DB_NAME -e "$MIGRATIONS; exec setup_1;"
 
 cargo +nightly run --release --package oj-gitlab-middleware --bin oj-gitlab-middleware
